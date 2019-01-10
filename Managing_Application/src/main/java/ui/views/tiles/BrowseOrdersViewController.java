ea6e2b361a0e71337da82ca3e76d8ed9ed205b0f
Kamil <kamil55525@gmail.com>
2019-01-10 23:24:17 +0100

    Reformat xmls in managing application and remove unnecessary elements

--- Managing_Application/src/main/java/ui/views/tiles/BrowseOrdersViewController.java
+++ Managing_Application/src/main/java/ui/views/tiles/BrowseOrdersViewController.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 import ui.Managing_Application;
 import ui.views.LoginView;
@@ -13,6 +16,17 @@ import ui.views.TileView;
 import java.io.IOException;
 
 public class BrowseOrdersViewController {
+    public ScrollPane scrollPane;
+    public TableView tableBrowseOrders;
+    public TableColumn TransactionDocumentColumn;
+    public TableColumn idColumn;
+    public TableColumn nameColumn;
+    public TableColumn addressColumn;
+    public TableColumn phoneColumn;
+    public TableColumn deliveryColumn;
+    public TableColumn statusColumn;
+    public TableColumn paymentColumn;
+
     public static void initialize() {
         try {
             @SuppressWarnings("ConstantConditions") final Parent parent = FXMLLoader.load(LoginView.class.getClassLoader().getResource("fxml/tiles/BrowseOrders.fxml"));
