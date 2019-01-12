903a5d69e746caa164f2cf0d13fac951a65d6b4f
Kamil <kamil55525@gmail.com>
2019-01-12 15:06:24 +0100

    Add trigger before delete on users table

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -5,3 +5,12 @@ CREATE TRIGGER storage_decrement AFTER INSERT ON orders_products FOR EACH ROW
         WHERE NEW.product_id = storage.product_id;
     END $$
 DELIMITER ;
+
+DELIMITER $$
+CREATE TRIGGER before_remove_user BEFORE DELETE  ON users FOR EACH ROW
+  BEGIN
+    IF OLD.user_id IN (SELECT user_id FROM  status_logs) THEN
+      SIGNAL SQLSTATE '40005' SET MESSAGE_TEXT = 'User is present in logs. Clean the logs to remove this user.' ;
+    END IF ;
+  END $$
+DELIMITER ;
