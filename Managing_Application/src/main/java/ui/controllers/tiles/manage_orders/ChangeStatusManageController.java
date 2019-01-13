04a88faa5784ad897232b1781ed6360b757ef0fa
dzazef <dzazef@gmail.com>
2019-01-13 01:04:02 +0100

    Updated enums in managing orders views.

--- Managing_Application/src/main/java/ui/controllers/tiles/manage_orders/ChangeStatusManageController.java
+++ Managing_Application/src/main/java/ui/controllers/tiles/manage_orders/ChangeStatusManageController.java
@@ -4,33 +4,25 @@ import db.LoginManager;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import model.Status;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 
 @SuppressWarnings("Duplicates")
 public class ChangeStatusManageController {
-    public ChoiceBox<String> chooseStatus;
+    public ChoiceBox<Status> chooseStatus;
     private int id;
     private Session session;
 
     public void initialize() {
-        chooseStatus.setItems(FXCollections.observableArrayList(
-                "in_progress", "prepared_for_sending", "sent", "delivered", "returned", "cancelled")
-        );
+        chooseStatus.setItems(FXCollections.observableArrayList(Status.values()));
         chooseStatus.getSelectionModel().selectFirst();
     }
 
     @FXML
     public void confirm() {
         int status = 0;
-        switch (chooseStatus.getValue()) {
-            case "in_progress" : status=0; break;
-            case "prepared_for_sending" : status=1; break;
-            case "sent" : status=2; break;
-            case "delivered" : status=3; break;
-            case "returned" : status=4; break;
-            case "cancelled" : status=5;
-        }
+        status = chooseStatus.getValue().ordinal();
         session.beginTransaction();
         Query query = session.
                 createSQLQuery("CALL music_store.update_order_status((:orderId), (:newStatus), (:userId))")
@@ -39,7 +31,7 @@ public class ChangeStatusManageController {
                 .setParameter("userId", LoginManager.getUsername());
         query.executeUpdate();
         session.getTransaction().commit();
-        ManageOrdersViewController.closeDialog(chooseStatus.getValue());
+        ManageOrdersViewController.closeDialog(chooseStatus.getValue().toString());
     }
 
     public void setInfo(int id, Session session) {
